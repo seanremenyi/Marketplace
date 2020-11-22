41351bed333d5e86d0ce26c0f105b2091898daf6
Sean <sean.remenyi@gmail.com>
2020-11-22 02:21:16 +0000

    user table added

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,8 @@
 from controllers.items_controller import items
+from controllers.auth_controller import auth
+
 
 registerable_controllers = [
+    auth,
     items
 ]
---
+++ src/controllers/auth_controller.py
@@ -0,0 +1,24 @@
+from models.User import User
+from schemas.UserSchema import user_schema
+from main import db
+from flask import Blueprint, request, jsonify, abort
+
+auth = Blueprint('auth', __name__, url_prefix="/auth")
+
+@auth.route("/register", methods=["POST"])
+def auth_register():
+    user_fields = user_schema.load(request.json)
+    
+    user = User.query.filter_by(email=user_fields["email"]).first()
+    
+    if user:
+        return abort(400, descrciption="Email already registered")
+        
+    user=User()
+    user.email = user_fields["email"]
+    user.password = user_fields["password"]
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify(user_schema.dump(user))
